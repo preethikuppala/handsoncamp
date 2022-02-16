25bf999aaacc3947b8f1aadb96888aef94334aaf
preethikuppala <99806867+preethikuppala@users.noreply.github.com>
2022-02-16 17:39:20 +0530

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace HandsOnProject
+{
+    
+    public class Client
+    {
+        #region gettersetters/accessors
+        public String ClientName { get; set; }
+        public long ClientPhNo { get; set; }
+        #endregion
+
+        #region client constructor
+        public Client(String name, long No)
+        {
+            ClientName = name;
+            ClientPhNo = No;
+        }
+        #endregion
+
+    }
+
+    public class ClientRegister
+    {
+        #region methods
+        public bool PhoneNumberValidation(string num)
+        {
+            string text = num;
+
+            if (!Regex.Match(text, @"^[0-9]{10}$").Success)
+            {
+                //Console.WriteLine("Invalid phone number");
+                return false;
+            }
+            else
+            {
+                //Console.WriteLine("valid Phone number");
+                return true;
+            }
+        }
+        #endregion
+
+    }
+
+
+    public class message
+    {
+        
+        server sr = new server();
+        #region methods
+        public void Messaging()
+        {
+            
+            Console.WriteLine("Please enter the UserName of the person you want to send message");
+            string name = Console.ReadLine();
+            foreach (var x in sr.clients)
+            {
+                if (x.ClientName == name)
+                {
+                    Console.WriteLine("Enter the message you want to send");
+                    string msg = Console.ReadLine();
+                    Message m = new Message(msg, ProgramServer.active_user , name);
+                    sr.MessagesList.Add(m);
+
+                }
+                else
+                {
+                    Console.WriteLine("There is no user with the name you entered");
+                }
+            }
+        }
+        #endregion methods
+
+    }
+
+
+    public class ContactBookInClient
+    {
+        public List<Client> ContactBook = new List<Client>();
+        server sr = new server();
+        #region methods
+        public void AddToContactBook(string n, long phno)
+            {
+                string num = phno.ToString();
+                ClientRegister cr = new ClientRegister();
+                if (cr.PhoneNumberValidation(num))
+                {
+
+                    Client cb1 = new Client(n, phno);
+                    if (!sr.CheckExistingUserOrNot(cb1))
+                        sr.clients.Add(cb1);
+                    ContactBook.Add(cb1);
+                }
+            }
+        
+
+        public void DisplayContactBook()
+            {
+                foreach (var x in ContactBook)
+                {
+                    Console.WriteLine(x.ClientName + " " + x.ClientPhNo);
+                }
+            }
+
+        #endregion
+    }
+
+
+}
+
+
+
---
+++ server.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnProject
+{
+    public class Message
+    {
+        #region GettersSetters
+
+        public String msg { get; set; }
+        public string sender { get; set; }
+        public string receiver { get; set; }
+
+        public DateTime dt { get; set; }
+        #endregion
+
+        #region constructors
+        public Message(String m, string s, string r)
+        {
+            msg = m;
+            sender = s;
+            receiver = r;
+            dt = DateTime.Now;
+        }
+        #endregion
+
+    }
+    public class server
+    {
+        //list that contains the customers details(All Data)
+
+        public List<Client> clients = new List<Client>();
+
+        public List<Message> MessagesList = new List<Message>();
+
+        #region methods
+        //checking UserExistingorNot in current clients list
+        public bool CheckExistingUserOrNot(Client client)
+        {
+            foreach (var x in clients)
+            {
+                if (x.ClientPhNo == client.ClientPhNo || x.ClientName == client.ClientName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        //To delete a existing user
+        public void DeleteExistingUser()
+        {
+            Console.WriteLine("please enter Registered user name : ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter the registered Phone Number");
+            long phno = Convert.ToInt64(Console.ReadLine());
+            Client cl = new Client(name, phno);
+            foreach (var x in clients.ToArray())
+            {
+                if (x.ClientPhNo == cl.ClientPhNo && x.ClientName == cl.ClientName)
+                {
+                    clients.Remove(x);
+                }
+            }
+        }
+
+
+
+        public void DisplayUsersDetails()
+        {
+            //print the list of clients
+            foreach (var x in clients)
+            {
+                Console.WriteLine(x.ClientName + " " + x.ClientPhNo);
+            }
+        }
+        #endregion
+
+    }
+}
